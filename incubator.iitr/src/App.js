f9e4658c800d0c34f69f31b36910f40f426f9e59
Amrit Prakash <amritdevil1st@gmail.com>
2023-09-18 01:34:29 +0530

    Initialize project

---
+++ incubator.iitr/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Starting the incubator.iitr project
+    </div>
+  );
+}
+
+export default App;
---
+++ incubator/incubator_iitr/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IncubatorIitrConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'incubator_iitr'
